76e2f746ea78751500ed9e9f519f291bc9835b36
Danilo <danilofranciscosilvaribeiro@gmail.com>
2026-04-23 09:54:26 -0300

    Locadora
    Criação e inicialização da atividade de java

---
+++ Locadora/src/Model/Cliente.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class Cliente {
+
+    String Cliente;
+
+    public Cliente(String cliente) {
+        Cliente = cliente;
+    }
+
+    public String getCliente() {
+        return Cliente;
+    }
+
+    public void setCliente(String cliente) {
+        Cliente = cliente;
+    }
+}
